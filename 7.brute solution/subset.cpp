d4167b2023259b8fc48d944b6f9d15117a8b2849
lilelr <lilelr@163.com>
2016-09-16 23:41:16 +0800

    位向量法求子集

--- 7.brute solution/subset.cpp
+++ 7.brute solution/subset.cpp
@@ -1,14 +1,5 @@
 // 求子集 如 {0，1，2}
-//
-// 3
-//
-// 0
-// 0 1
-// 0 1 2
-// 0 2
-// 1
-// 1 2
-// 2
+
 #include <algorithm>
 #include <cmath>
 #include <cstdio>
@@ -25,6 +16,16 @@ using namespace std;
 const int maxn = 100;
 
 //增量法求子集
+//
+// 3
+//
+// 0
+// 0 1
+// 0 1 2
+// 0 2
+// 1
+// 1 2
+// 2
 void print_subset(int n, int* A, int cur) {
   for (int i = 0; i < cur; i++) {
     cout << A[i] << " ";  //打印当前集合
@@ -37,8 +38,33 @@ void print_subset(int n, int* A, int cur) {
   }
 }
 
+// 0 1 2
+// 0 1
+// 0 2
+// 0
+// 1 2
+// 1
+// 2
+//位向量法求子集
+void print_subset2(int n, int* B, int cur) {
+  if (cur == n) {
+    for (int i = 0; i < cur; i++) {
+      if (B[i]) {
+        cout << i << " ";
+      }
+    }
+    cout << endl;
+    return;
+  }
+  B[cur] = 1;
+  print_subset2(n, B, cur + 1);  //选第cur个元素
+  B[cur] = 0;
+  print_subset2(n, B, cur + 1);  // 不选第cur个元素
+}
+
 int n;
 int nums[maxn];
+int visited[maxn];
 int main() {
   //  freopen("", "r", stdin);
   //  freopen("", "w", stdout);
@@ -48,7 +74,7 @@ int main() {
   t = 1;
   while (T--) {
     cin >> n;
-    print_subset(n, nums, 0);
+    print_subset2(n, visited, 0);
   }
   return 0;
 }

5bd55488878fce454344d492309f651f1f38afc6
lilelr <lilelr@163.com>
2016-10-08 11:01:29 +0800

    枚举

--- 7.brute solution/129 - Krypton Factor.cpp
+++ 7.brute solution/129 - Krypton Factor.cpp
@@ -67,43 +67,26 @@ bool is_repeat_next(string str) {
 //  }
 //}
 
-int relation_str(string str) {
-  int ok = 1;
-  int len = str.length() - 1;
-  for (int j = 1; j * 2 <= len; j++) {  // 尝试长度为j*2的后缀
-    int equal = 1;
-    for (int k = 0; k < j; k++)  // 检查后一半是否等于前一半
-      if (str[len - k] != str[len - k - j]) {
-        equal = 0;
-        break;
-      }
-    if (equal) {
-      ok = 0;
-      break;
-    }  // 后一半等于前一半，方案不合法
-  }
-  return ok;
-}
-
-void find_hard(int cur, string str) {
-  total++;
-  if (total == n) {
-    ans = str;
-    return;
-  } else if (total > n) {
-    return;
-  } else {
-    for (int i = 0; i < L; i++) {
-      char add_letter = 'A' + i;
-      str += add_letter;
-      if (relation_str(str)) {
-        find_hard(cur + 1, str);
-      } else {
-        str.erase(str.length() - 1, 1);
-      }
-    }
-  }
-}
+//
+// void find_hard(int cur, string str) {
+//  total++;
+//  if (total == n) {
+//    ans = str;
+//    return;
+//  } else if (total > n) {
+//    return;
+//  } else {
+//    for (int i = 0; i < L; i++) {
+//      char add_letter = 'A' + i;
+//      str += add_letter;
+//      if (relation_str(str)) {
+//        find_hard(cur + 1, str);
+//      } else {
+//        str.erase(str.length() - 1, 1);
+//      }
+//    }
+//  }
+//}
 
 int cnt;
 int s_int[100];
@@ -123,58 +106,104 @@ int dfs(int cur) {
 
   for (int i = 0; i < L; i++) {
     s_int[cur] = i;
+
+    //只需判断当前串的后缀是否可能重复，如“EABCABC"
+    //存在重复，自然不会出现”EABCABCF"的下一步的子串
     int ok = 1;
-    for (int j = 1; j * 2 <= cur + 1; j++) {  // 尝试长度为j*2的后缀
+    for (int j = 1; j * 2 <= cur + 1; j++) { // 尝试长度为j*2的后缀
       int equal = 1;
-      for (int k = 0; k < j; k++)  // 检查后一半是否等于前一半
+
+      for (int k = 0; k < j; k++) // 检查后一半是否等于前一半
         if (s_int[cur - k] != s_int[cur - k - j]) {
           equal = 0;
           break;
         }
+
       if (equal) {
         ok = 0;
         break;
-      }  // 后一半等于前一半，方案不合法
+      } // 后一半等于前一半，方案不合法
     }
     if (ok)
-      if (!dfs(cur + 1)) return 0;
+      if (!dfs(cur + 1))
+        return 0;
   }
   return 1;
 }
 
-void solve() {
-  n++;
-  total = 0;
-  ans = "";
-  str = "";
-  find_hard(-1, str);
+int my_dfs(int cur) {
+  if (cnt++ == n) {
+    for (int i = 0; i < cur; i++) {
+      if (i % 64 == 0 && i > 0) {
+        printf("\n");
+      } else if (i % 4 == 0 && i > 0) {
+        printf(" ");
+      }
+      printf("%c", 'A' + s_int[i]);
+    }
+    printf("\n%d\n", cur);
+    return 0;
+  } else {
+    for (int i = 0; i < L; i++) {
+      s_int[cur] = i;
 
-  int ans_len = ans.length();
-  while (!ans.empty()) {
-    for (int i = 0; i < 16; i++) {
-      if (i == 0) {
-        cout << ans.substr(0, 4);
-        ans.erase(0, 4);
-      } else {
-        cout << " " << ans.substr(0, 4);
-        ans.erase(0, 4);
+      int ok = 1;
+      for (int j = 1; j * 2 < cur + 1; j++) {
+        int equal = 1;
+        for (int k = 0; k < j; k++) {
+          if (s_int[cur - k] != s_int[cur - k - j]) {
+            equal = 0;
+            break;
+          }
+        }
+        if (equal) {
+          ok = 0;
+          break;
+        }
+      }
+      if (ok) {
+        if (!dfs(cur + 1)) {
+          return 0;
+        }
       }
     }
-    cout << endl;
   }
-  cout << ans_len << endl;
+  return 1;
 }
 
+// void solve() {
+//  n++;
+//  total = 0;
+//  ans = "";
+//  str = "";
+//  find_hard(-1, str);
+//
+//  int ans_len = ans.length();
+//  while (!ans.empty()) {
+//    for (int i = 0; i < 16; i++) {
+//      if (i == 0) {
+//        cout << ans.substr(0, 4);
+//        ans.erase(0, 4);
+//      } else {
+//        cout << " " << ans.substr(0, 4);
+//        ans.erase(0, 4);
+//      }
+//    }
+//    cout << endl;
+//  }
+//  cout << ans_len << endl;
+//}
+
 int main() {
-//  freopen("/Users/yuxiao/XcodeProject/7.brute solution/7.brute solution/in",
-//          "r", stdin);
-//  freopen("/Users/yuxiao/XcodeProject/7.brute solution/7.brute solution/out",
-//          "w", stdout);
+    freopen("/Users/yuxiao/XcodeProject/7.brute solution/7.brute solution/in",
+            "r", stdin);
+    freopen("/Users/yuxiao/XcodeProject/7.brute solution/7.brute solution/out",
+            "w", stdout);
 
   while (cin >> n >> L && (n != 0 && L != 0)) {
     cnt = 0;
-    dfs(0);
-    //    cout << relation_str("ABACABAC") << endl;
+    my_dfs(0);
+    //    cout << relation_str("EABCABCF") << endl;
     //    cout << is_repeat_next("CABCAB") << endl;
     //    cout << is_repeat_next("CABB") << endl;
     //    cout << is_repeat_next("CAB") << endl;
---
+++ 7.brute solution/Uva12325 Zobie's Treasure Chest.cpp
@@ -0,0 +1,49 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+#define LL long long
+
+int main() {
+  freopen("/Users/yuxiao/XcodeProject/7.brute solution/7.brute solution/in",
+          "r", stdin);
+  freopen("/Users/yuxiao/XcodeProject/7.brute solution/7.brute solution/out",
+          "w", stdout);
+
+  int T, t;
+  cin >> T;
+  t = 1;
+  LL ans;
+  while (T--) {
+    int n, s1, s2, v1, v2;
+    cin >> n >> s1 >> v1 >> s2 >> v2;
+    if (s1 > s2) { // s2 代表体积大的，s1 代表体积小的
+      swap(s1, s2);
+      swap(v1, v2);
+    }
+    ans = 0;
+    if (n / s2 >= 65536) {
+      for (LL i = 0; i <= s1; i++) { // s2 体积的物品不会超过s1
+        ans = max(ans, v2 * i + (n - s2 * i) / s1 * v1);
+      }
+      for (LL i = 0; i <= s2; i++) {
+        ans = max(ans, v1 * i + (n - s1 * i) / s2 * v2);
+      }
+    } else { // s2 is large
+      for (LL i = 0; s2 * i <= n; i++) {
+        ans = max(ans, v2 * i + (n - s2 * i) / s1 * v1);
+      }
+    }
+
+    printf("Case #%d: %lld\n", t++, ans);
+  }
+  return 0;
+}
